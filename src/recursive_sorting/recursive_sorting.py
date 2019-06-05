795e23ffc534a16634c2f42f3c19ef1f92c33eab
IVB107 <ianvb107@gmail.com>
2019-06-05 16:16:40 -0500

    Finally found my bug and completed merge sort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,42 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
 def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
-    merged_arr = [0] * elements
-    # TO-DO
-    
+    merged_arr = []
+
+    l, r = 0, 0
+    while l < len(arrA) and r < len(arrB):
+
+        if arrA[l] < arrB[r]:
+            merged_arr.append(arrA[l])
+            l += 1
+        elif arrB[r] < arrA[l]:
+            merged_arr.append(arrB[r])
+            r += 1
+
+    while r < len(arrB):
+        merged_arr.append(arrB[r])
+        r += 1
+    while l < len(arrA):
+        merged_arr.append(arrA[l])
+        l += 1
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
-    # TO-DO
+def merge_sort( arr ):    
+    if len(arr) <= 1:  # base case
+        return arr
+        
+    half = len(arr)//2
+    left = merge_sort(arr[:half])
+    right = merge_sort(arr[half:])
 
-    return arr
+    print('Merged Arr: ', merge(left, right))
+    return merge(left, right)
+
+    # return arr
+
+merge_sort([3, 5, 12, 1, 23, 22, 13, 4, 7, 2, 19, 6, 21, 14, 25, 34] )
 
 
 # STRETCH: implement an in-place merge sort algorithm
